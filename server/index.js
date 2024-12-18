8b6b7b83649875131908da65d9ebbc816f8565ca
vikesh kumar mahto <kumarvikesh287@gmail.com>
2024-12-18 15:14:44 +0530

    Fix scripts and deployment issues

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(_dirname, "/client/dist")));
+app.use(express.static(path.join(_dirname, "client", "dist")));
 app.get("*", (_, res) => {
   res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
 });
