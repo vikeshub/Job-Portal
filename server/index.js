7144c3e528468c65d2978174f3323b5929ce0881
vikesh kumar mahto <kumarvikesh287@gmail.com>
2024-12-19 14:51:03 +0530

    url change

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://job-portal-s53j.onrender.com",
   credentials: true, // Corrected typo
 };
 app.use(cors(corsOption));
